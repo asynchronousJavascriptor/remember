77b6b1b6887cd6bb64f03e2d6af613610ab2b497
Harsh Sharma <harshu8545@gmail.com>
2023-12-18 18:46:01 +0530

    code before 18 dec

--- script2.js
+++ script2.js
@@ -1,7 +1,34 @@
 const close = document.querySelector("#closebtn");
 const overlay = document.querySelector("#overlay");
+const makebtn = document.querySelector("#makebtn");
+const maketask = document.querySelector("#maketask");
 
+localStorage.setItem("tasks", stringi([]));
 
 close.addEventListener("click", function(){
-    overlay.style.opacity = 0;
+    overlay.style.display = "none";
 });
+
+makebtn.addEventListener("click", function(){
+    overlay.style.display = "initial";
+});
+
+maketask.addEventListener("click", function(){
+    var valueoftitle = document.querySelector("#title").value;
+    var valueofdata = document.querySelector("#data").value;
+    
+    const ans = parser(localStorage.getItem("tasks"));
+    ans.push({title: valueoftitle, data: valueofdata})
+
+    localStorage.setItem("tasks", stringi(ans));
+});
+
+
+function parser(data){
+    return JSON.parse(data);
+}
+
+function stringi(data){
+    return JSON.stringify(data);
+}
+
